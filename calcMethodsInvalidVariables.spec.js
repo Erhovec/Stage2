538ff79966b4e5702e9401185c7a36763c4cb8c3
Kate <kate.erhovec@gmail.com>
2021-12-15 22:11:49 +0300

    changed mistakes

---
+++ calcMethodsInvalidVariables.spec.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+const Calculator = require("../../app/calculator.js");
+const { expect } = require("chai");
+
+describe("Calculator", function () {
+  let operation = new Calculator();
+
+  it("Should throw an error if not a number argument was passed to add method", function () {
+    let isErrorMessageAppeared = () => operation.add(5, "ht");
+    expect(isErrorMessageAppeared).to.throw("Variable should be a number!");
+  });
+  it("Should throw an error if not a number argument was passed to add method", function () {
+    let isErrorMessageAppeared = () => operation.multiply("letter", 10);
+    expect(isErrorMessageAppeared).to.throw("Variable should be a number!");
+  });
+});
---
+++ calcMethodsValidVariables.spec.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+const Calculator = require('../../app/calculator.js');
+const {expect} = require('chai');
+
+describe('calcMethodsValidVariables', function() {
+  let operation = new Calculator;
+  
+  it("should return 7 when add 2 to 5", function() {
+    expect(operation.add(2, 5)).to.be.equal(7);
+  });
+
+  it("should return 100 when multiply 25 by 4", function() {
+    expect(operation.multiply(25, 4)).to.be.equal(100);
+  });
+});
